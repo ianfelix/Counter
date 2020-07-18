f8afe17f24292bbbfb209d9a0c37aa583c4f3ae3
Ian Felix <ianfelix030@gmail.com>
2020-07-18 02:31:35 -0300

    counter created with react, css and react icons
    React app counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { FaPlus, FaMinus, FaRedo, FaReact } from 'react-icons/fa';
+import './style.css';
 
 function App() {
+  const [click, setClick] = useState(0);
+
+  const handleClickUp = () => {
+    setClick(click + 1);
+  };
+  const handleClickDown = () => {
+    setClick(click - 1);
+  };
+
+  const handleClickReset = () => {
+    setClick(0);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <FaReact className="react" />
+        <h1>Contador</h1>
+        <span className="count">{click}</span>
+        <button className="up" onClick={handleClickUp}>
+          <FaPlus />
+        </button>
+        <button className="down" onClick={handleClickDown}>
+          <FaMinus />
+        </button>
+        <button className="reset" onClick={handleClickReset}>
+          <FaRedo />
+        </button>
+      </div>
     </div>
   );
 }
